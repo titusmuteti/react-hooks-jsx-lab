31fbc5364d27fa3c03617aaa5a93bd9fa85d8d4d
Titus Mumo <titusmumo@gmail.com>
2022-09-19 23:41:07 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>;
 }
 
 export default NavBar;
